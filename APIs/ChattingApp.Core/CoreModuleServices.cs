170eba505a4149d4c3d2a42accfcb3a892dfe22d
khaled979797 <khaled.alshreef25@gmail.com>
2024-10-26 14:02:37 +0300

    Adding register and login endpoints and using JWT for authentication

---
+++ APIs/ChattingApp.Core/CoreModuleServices.cs
@@ -0,0 +1,48 @@
+﻿using ChattingApp.Core.Context;
+using ChattingApp.Core.Interfaces;
+using ChattingApp.Core.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ChattingApp.Core
+{
+    public static class CoreModuleServices
+    {
+        public static IServiceCollection AddCoreModuleServices(this IServiceCollection services, IConfiguration config)
+        {
+            #region Context
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("dbContext"));
+            });
+            #endregion
+
+            #region Cors
+            services.AddCors();
+            #endregion
+
+            #region Dependencies
+            services.AddScoped<ITokenRepository, TokenRepository>();
+            #endregion
+
+            #region JWT
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            #endregion
+
+            return services;
+        }
+    }
+}
---
+++ APIs/ChattingApp.Core/Interfaces/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using ChattingApp.Entities.Models;
+
+namespace ChattingApp.Core.Interfaces
+{
+    public interface ITokenRepository
+    {
+        string CreateToken(AppUser user);
+    }
+}
